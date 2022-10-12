f7d093ca8b1162ff53729bb0b9724b7cd3c8eea1
Axel Donath <axel.donath@cfa.harvard.edu>
2022-10-11 20:24:18 -0400

    Update release pipeline

---
+++ dev/prepare-release.py
@@ -0,0 +1,35 @@
+from os import stat
+
+
+import click
+import pathlib
+
+
+def reserve_doi():
+    # Use Zenodo API to reserve a DOI
+    pass
+
+
+def update_citation_cff():
+    # update author list according to PIG 24
+    # Add reseced DOI
+    # Adapt version and release date
+    pass
+
+
+def update_gh_release_template():
+    pass
+
+
+def cli(release):
+    pass
+    
+
+if __name__ == "__main__":
+    cli()
+    print("Please review and commit the chnages now.")
+
+
+
+
+
